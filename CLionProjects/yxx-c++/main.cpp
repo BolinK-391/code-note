17a9f782fed4a48829377ef6984479350ad86ba3
YXX <448739833@qq.com>
2024-03-21 20:37:44 +0800

    操作运算符

--- CLionProjects/yxx-c++/main.cpp
+++ CLionProjects/yxx-c++/main.cpp
@@ -1,1 +1,22 @@
-#include<studio.h>
+#include <stdio.h>
+//计算n的阶乘
+int main() {
+    int i,j=1,z=1;
+    scanf("%d",&i);
+    for(int num = 1;num<=i;num++){
+    //     z = z*num;
+    //     j = z ;
+    //简便写法 将z乘上num后再把乘完之后的数字赋给z！
+        z *= num ;
+
+        printf("%d",num);
+        if(num == i){
+            continue;
+        }
+        printf("*");
+    }
+    printf("=%d",z);
+    }
+
+
+
