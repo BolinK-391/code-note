70564284238c6c6c0d2921e8b31aeff3a7d371a3
BolinK-391 <163728735+BolinK-391@users.noreply.github.com>
2024-08-05 17:40:00 +0800

    change new file

---
+++ C++/stack类（适配器模拟）/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+using namespace std;
+
+namespace my_stack
+{
+    template<class T,class Container> // <类型,容器>
+    class stack
+    {
+    public:
+        void push(const T& x)
+        {
+            _con.push_back(x);
+        }
+
+        void pop()
+        {
+            _con.pop_back();
+        }
+
+        size_t size()
+        {
+            return _con.size();
+        }
+
+        bool empty()
+        {
+            return _con.empty();
+        }
+
+        T& top()
+        {
+            return _con.back();
+        }
+    private:
+        Container _con;
+    };
+
+    void test_stack()
+    {
+        stack<int,vector<int>> st;
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+
+        while(!st.empty())
+        {
+            cout << st.top() << " ";
+            st.pop();
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    my_stack::test_stack();
+    return 0;
+}
